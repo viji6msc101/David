695a37b36c049e957bfa6f74d952eb5db11b6d33
viji6msc101 <viji6msc101@gmail.com>
2019-03-01 16:46:11 +0530

    Test1

--- GitPrograme1/src/GitPrograme.java
+++ GitPrograme1/src/GitPrograme.java
@@ -1,12 +1,12 @@
 
-public class David {
+public class GitPrograme {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.println("1");
 
-		
-		
-		System.out.println("David");
 	}
 
 }
